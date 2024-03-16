9d36b8a786ff579180fa0300ddddc19790d9f42b
Gustavo <yusgus02@gmail.com>
2024-03-15 23:21:40 -0600

    Subir Base De Datos

---
+++ SGPM_DataBAse/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGPM_DataBAse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
