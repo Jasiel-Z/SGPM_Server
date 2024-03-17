48df48ce2325399ad6b72f6ca02cdc858c330e3d
Jasiel-Z <zS21013907@estudiantes.uv.mx>
2024-03-17 17:12:25 -0600

    Creación de clases de la base de datos

--- SGPM_DataBAse/DataBaseModel.Designer.cs
+++ SGPM_DataBAse/DataBaseModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\jasie\Source\Repos\SGPM_Server\SGPM_DataBAse\DataBaseModel.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\jasie\source\repos\SGPM_Server\SGPM_DataBAse\DataBaseModel.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
