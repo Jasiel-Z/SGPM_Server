53b98d376c22ffc184f525e0c9059895d06d63ac
Jasiel-Z <zS21013907@estudiantes.uv.mx>
2024-03-19 10:32:05 -0600

    Eliminación de la relación de beneficiario con localidad, de lado de localidad

--- SGPM_DataBAse/Localidad.cs
+++ SGPM_DataBAse/Localidad.cs
@@ -24,7 +24,6 @@ namespace SGPM_DataBAse
     
         public int IdLocalidad { get; set; }
         public string nombre { get; set; }
-        public string BeneficiarioRFC { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Beneficiario> Beneficiario { get; set; }
