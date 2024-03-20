351d299efca4e8fef4dcb8a6173482e542111e04
Jesusmjc <121887458+Jesusmjc@users.noreply.github.com>
2024-03-20 00:08:38 -0600

    wip UserManagement

---
+++ SGPM_Contracts/IUserManagement/IUserManagement.cs
@@ -0,0 +1,34 @@
+﻿using SGPM_DataBAse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGPM_Contracts.IUserManagement
+{
+    [ServiceContract]
+    public interface IUserManagement
+    {
+        [OperationContract]
+        int SaveUser(User user);
+    }
+
+    [DataContract]
+    public class User
+    {
+        [DataMember]
+        public int UserID { get; set; }
+
+        [DataMember]
+        public string Email { get; set; }
+
+        [DataMember]
+        public string Password { get; set; }
+
+        [DataMember]
+        public int StaffNumber {  get; set; }
+    }
+}
---
+++ SGPM_Services/UserManagement/UserManager.cs
@@ -0,0 +1,51 @@
+﻿using SGPM_Contracts.IUserManagement;
+using SGPM_DataBAse;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core;
+using System.Data.Entity.Validation;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGPM_Services.UserManagement
+{
+    public partial class SGPMManager : IUserManagement
+    {
+        public int SaveUser(User user)
+        {
+            int result;
+            try
+            {
+                using (var context = new DataBaseModelContainer())
+                {
+                    UsuarioSet userToBeSaved = new UsuarioSet();
+                    userToBeSaved.correo = user.Email;
+                    userToBeSaved.contrasena = user.Password;
+                    //userToBeSaved.EmpleadoSet = user.StaffNumber;
+                    
+                    context.UsuarioSet.Add(userToBeSaved);
+                    result = context.SaveChanges();
+                }
+            }
+            catch (SqlException exception)
+            {
+                Console.WriteLine(exception.Message);
+                result = -1;
+            }
+            catch (DbEntityValidationException exception)
+            {
+                Console.WriteLine(exception.Message);
+                result = -1;
+            }
+            catch (EntityException exception)
+            {
+                Console.WriteLine(exception.Message);
+                result = -1;
+            }
+
+            return result;
+        }
+    }
+}
