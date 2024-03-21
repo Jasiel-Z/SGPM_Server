a777c9b3778c923c5fbef1f7b63b191696e097d4
Gustavo <yusgus02@gmail.com>
2024-03-21 05:12:25 -0600

    Subir Primeros Cambios

--- SGPM_Contracts/IProjectsManagement/IProjectsManagement.cs
+++ SGPM_Contracts/IProjectsManagement/IProjectsManagement.cs
@@ -15,10 +15,17 @@ namespace SGPM_Contracts.IProjectsManagement
         [OperationContract]
         Project GetProjectDetails(int idProject);
 
+        [OperationContract]
+        int RegisteredProjects(Project project);
 
+        [OperationContract]
+        List<Project> GetAllProjects();
 
+        [OperationContract]
+        List<Localidad> GetLocalidads();
 
-
+        [OperationContract]
+        List<Dependency> GetDependencies();
     }
 
     [DataContract]
@@ -27,13 +34,63 @@ namespace SGPM_Contracts.IProjectsManagement
         [DataMember]
         public int Folio {  get; set; }
 
+        [DataMember] 
+        public string Name { get; set; }
+
+        [DataMember] 
+        public string Description { get; set; }
+
+        [DataMember] 
+        public int SupportAmount{ get; set; }
+        
         [DataMember]
-        public string  Modality { get; set; }
+        public string Location { get; set; }
+
+        [DataMember]
+        public string Dependecy { get; set; }
+
+        [DataMember]
+        public string State { get; set; }
+
+        [DataMember]
+        public string Status { get; set; }
+
+        [DataMember]
+        public string Modality { get; set; }
 
         [DataMember]
         public string AttentionGroup { get; set; }
 
         [DataMember]
         public int BeneficiaryNumbers { get; set; }
+
+        [DataMember]
+        public DateTime Start {  get; set; }
+
+        [DataMember]
+        public DateTime End { get; set; }
+
+        [DataMember]
+        public DateTime Evidence { get; set; }
+    }
+
+    [DataContract]
+    public class Localidad
+    {
+        [DataMember]
+        public int IdLocalidad { get; set; }
+
+        [DataMember]
+        public string NameLocalidad { get; set;  }
+    }
+
+    [DataContract]
+    public class Dependency
+    {
+        [DataMember]
+        public int IdDependency { get; set; }
+
+        [DataMember]
+        public string NameDependency { get; set; }
     }
 }
--- SGPM_Services/ProjectsManagement/ProjectManager.cs
+++ SGPM_Services/ProjectsManagement/ProjectManager.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Data.Entity.Core;
+using System.Data.Entity.Migrations;
 using System.Data.Entity.Validation;
 using System.Data.SqlClient;
 using System.Linq;
@@ -18,6 +19,86 @@ namespace SGPM_Services.ProjectsManagement
 
     public partial class SGPMManager : IProjectsManagement
     {
+        public List<Project> GetAllProjects()
+        {
+            List<Project> projects = new List<Project>();
+
+            try
+            {
+                using (var context = new DataBaseModelContainer())
+                {
+                    var proyectos = context.ProyectoSet.ToList();
+                    foreach (var proyecto in proyectos)
+                    {
+                        Project project = new Project
+                        {
+                            Folio = proyecto.Folio,
+                            Modality = proyecto.modalidad,
+                            AttentionGroup = proyecto.grupoAtencion,
+                            BeneficiaryNumbers = proyecto.numeroBeneficiarios
+                        };
+                        projects.Add(project);
+                    }
+                    return projects;
+                }
+            }
+            catch (SqlException exception)
+            {
+                Console.WriteLine(exception.Message);
+                return null;
+            }
+            catch (DbEntityValidationException exception)
+            {
+                Console.WriteLine(exception.Message);
+                return null;
+            }
+            catch (EntityException exception)
+            {
+                Console.WriteLine(exception.Message);
+                return null;
+            }
+        }
+
+        public List<Dependency> GetDependencies()
+        {
+            List<DependenciaSet> dependencies = new List<DependenciaSet>(); 
+            List<Dependency> dependenciesList = new List<Dependency>();
+
+            using(var context = new DataBaseModelContainer())
+            {
+                dependencies = context.DependenciaSet.ToList();
+                foreach(var dep in dependencies)
+                {
+                    Dependency dependency = new Dependency();
+                    dependency.IdDependency = dep.IdDependencia;
+                    dependency.NameDependency = dep.nombre;
+                    dependenciesList.Add(dependency);
+                }
+            }
+
+            return dependenciesList;
+        }
+
+        public List<Localidad> GetLocalidads()
+        {
+            List<LocalidadSet> localidads = new List<LocalidadSet>();
+            List<Localidad> localidadList = new List<Localidad>();
+
+            using (var context = new DataBaseModelContainer())
+            {
+                localidads = context.LocalidadSet.ToList();
+                foreach (var loc in localidads)
+                {
+                    Localidad dependency = new Localidad();
+                    dependency.IdLocalidad = loc.IdLocalidad;
+                    dependency.NameLocalidad = loc.nombre;
+                    localidadList.Add(dependency);
+                }
+            }
+
+            return localidadList;
+        }
+
         public Project GetProjectDetails(int idProject)
         {
             Project project = new Project();   
@@ -51,10 +132,44 @@ namespace SGPM_Services.ProjectsManagement
             }
         }
 
+        public int RegisteredProjects(Project project)
+        {
+            int result = 1;
+            if (project == null)
+            {
+                try
+                {
+                    using (var context = new DataBaseModelContainer())
+                    {
+                        ProyectoSet proyecto = new ProyectoSet();
+                        proyecto.Folio = project.Folio;
+                        proyecto.grupoAtencion = project.AttentionGroup;
+                        proyecto.estado = project.Status;
+                        proyecto.tipo = project.Modality;
+                        proyecto.fechaFin = project.End;
+                        proyecto.fechaLimiteEvidencia = project.Evidence;
+                        proyecto.fechaLimiteSolicitud = project.Start;
 
+                        context.ProyectoSet.AddOrUpdate(proyecto);
+                        context.SaveChanges();
+                        result = 0;
+                    }
+                }
+                catch (SqlException exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+                catch (DbEntityValidationException exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+                catch (EntityException exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+            }
 
-        
-
-        
+            return result;
+        }
     }
 }
