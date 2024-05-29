44244829ccb0ca54cf046564079cf5df5882e0ae
Gustavo <yusgus02@gmail.com>
2024-05-29 00:24:35 -0600

    Politicas!

--- SGPM_Services/PolicyManagement/PolicyManager.cs
+++ SGPM_Services/PolicyManagement/PolicyManager.cs
@@ -14,6 +14,37 @@ namespace SGPM_Services.ProjectsManagement
 {
     public partial class SGPMManager : IPolicyManagement
     {
+        public List<Policy> GetAllPolicies()
+        {
+            List<Policy> policyList = new List<Policy>();
+
+            try
+            {
+                using (var context = new SGPMEntities())
+                {
+                    var policyListDB = context.PoliticasOtorgamiento.ToList();
+                    foreach (var policy in policyListDB)
+                    {
+                        Policy newPolicy = new Policy
+                        {
+                            PolicyID = policy.IdPoliticaOtorgamiento,
+                            Name = policy.nombre,
+                            Description = policy.descripcion
+                        };
+
+                        policyList.Add(newPolicy);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+
+            return policyList;
+        }
+
         public int SavePolicy(Policy policy)
         {
             int result = 0;
@@ -46,6 +77,107 @@ namespace SGPM_Services.ProjectsManagement
             }
 
             return result;
+        }
+
+        public int AddPolicyToProject(string folio, List<int> listPolicys)
+        {
+            int result = 0;
+            try
+            {
+                using (var context = new SGPMEntities())
+                {
+                    DeletePolicyOfProject(folio);
+                    foreach (var idPolicy in listPolicys)
+                    {
+                        ProyectoPoliticaOtorgamiento policy = new ProyectoPoliticaOtorgamiento
+                        {
+                            IdPoliticaOtorgamiento = idPolicy,
+                            Folio = folio
+                        };
+                        context.ProyectoPoliticaOtorgamiento.Add(policy);
+                    }
+                    result = context.SaveChanges();
+                }
+            }
+            catch (SqlException exception)
+            {
+                Console.WriteLine(exception.Message);
+                result = -1;
+            }
+            catch (DbEntityValidationException exception)
+            {
+                Console.WriteLine(exception.Message);
+                result = -1;
+            }
+            catch (EntityException exception)
+            {
+                Console.WriteLine(exception.Message);
+                result = -1;
+            }
+
+            return result; 
+        }
+
+        public List<int> GetPolicysOfProject(string idProject)
+        {
+            List<int> listPolicy = new List<int>();
+            try
+            {
+                using (var context = new SGPMEntities())
+                {
+                    var policyDB = context.ProyectoPoliticaOtorgamiento.Where(p => p.Folio == idProject).ToList();
+                    if (policyDB != null)
+                    {
+                        foreach (var policy in policyDB)
+                        {
+                            listPolicy.Add((int)policy.IdPoliticaOtorgamiento);
+                        }
+                    }
+                }
+            }
+            catch (SqlException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            catch (DbEntityValidationException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            catch (EntityException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+
+            return listPolicy;
+        }
+
+        private void DeletePolicyOfProject(string idProject)
+        {
+            try
+            {
+                using (var context = new SGPMEntities())
+                {
+                    var policiesToDelete = context.ProyectoPoliticaOtorgamiento.Where(p => p.Folio == idProject).ToList();
+
+                    if (policiesToDelete != null)
+                    {
+                        context.ProyectoPoliticaOtorgamiento.RemoveRange(policiesToDelete);
+                        context.SaveChanges();
+                    }
+                }
+            }
+            catch (SqlException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            catch (DbEntityValidationException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            catch (EntityException exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
         }
     }
 }
